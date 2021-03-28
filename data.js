1fb4f555b449afa521b0b2121ed2019a46877894
Dong-Store <dongnv2902@gmail.com>
2021-03-28 18:22:30 +0700

    data

---
+++ data.js
@@ -0,0 +1,59 @@
+[{
+    "id": "1",
+    "image": [
+        {
+            "url": "https://sat02pap001files.storage.live.com/y4mvbske3lfbkJlMHRoz9b5_M-Qrv45Rf2Y3namJ-7QgdIpPc9vsgDsqUM4sTdgEcjqO1NkcUf_YGVZwB5PENAg4KnJt6arxKGHcw-mZdxYVch2vWpp2LMJwGXfFpBup6_sFNBjhzNcn9kj4_w_-Z9ikVYwJrDl_dquGIRW4xG3aIfCax1hc_ZEOCmS9PklGvmy?width=576&height=576&cropmode=none",
+            "form": "1"
+        }, {
+            "url": "https://sat02pap001files.storage.live.com/y4mgNXuF1_8xMm_KpWQpBAft9aPkp5N7FIh3nvzwIAyw9QZPMVvyZlnHarfz37Fga2EgD1K9JR0ObSzEnTxegF2tkPI4bStQX-TvfW2BNzwpd7V1OuGiLmsbme4k2Fop6yv-WTkX81GI0gtXZYPbGdFDrcjlA33TEw4dWGrWz2_HhfZeKV7rqPhFhpEcK13GFLD?width=576&height=576&cropmode=none",
+            "form": "1"
+        }, {
+            "url": "https://sat02pap001files.storage.live.com/y4mcAN_DXA6Xqk5W8eYpbXEMaIFd3AyQ9fjZzWhF6Zw1bMVb8E1WHKEo_lCX8Z5DOeDktrKLuDHcWnXDQl5NzUdtNR7LMroGwgp3hLZmaqgLQDkRqTKNDq6dMKY3V9uX3IVqWVl5Iu2EnwqVQQx8E1l5srmYyg6VBhNos8rOTLXKl1yN1qCEYGqGL31LIA1tFhR?width=576&height=576&cropmode=none",
+            "form": "2"
+        }, {
+            "url": "https://sat02pap001files.storage.live.com/y4mJFLGYCODYu7P1ZSduHMqRPC1dPL8w_42SDpeEMfDB-tnKCSvt8Eu0OAitrlOUmWUMrqs7N-GhpzjVvol4SrkN1XIcvZRpvMVmxqLeG9FSsK5ny73Y-KxdcQW6F4Z9JwQA4ykVPHgPnZsGbqCB5XklQ_zcm954n4USykduF_Nj0S_tfAPn8j5M7TCeTWPRv1j?width=576&height=576&cropmode=none",
+            "form": "2"
+        }, {
+            "url": "https://sat02pap001files.storage.live.com/y4mWY4WRD29xLDDPJ86022h7qRPzSPT182EkYj1cGukHTAXR1MByxQHB4wmSwUdcewbkQqF-MP9cWDRf8Zv2TwDCSEpUd_xBQwybqNrD-TyKOmTXaX2hlfXLwMX9_ze1mcD93JIngjR91cKGtJwQVezR4kGFlBKiDnilPfU-NNWaT_fFmgtnPSgEjc2NRDE5uBu?width=576&height=461&cropmode=none",
+            "form": "3"
+        }, {
+            "url": "https://sat02pap001files.storage.live.com/y4mxvazwDZmlLn0lb_IkJeXLprfUhEW2CEnl187Iuk95mjmAW1ILZBz5R5I_YQyiir4CKe9ELoBdx1Wf6TziLtCupmBFBKokrsaJYGeWYjUKWQJCV-9G0VqfQFFLsR1fkhZBroNcdTEbPxtYHBSck8ybF6Lt_xqMVG_BkIsHdRY1XVPc_sQQ_RmO4HKZfAq9B9e?width=576&height=325&cropmode=none",
+            "form": "3"
+        }, {
+            "url": "https://sat02pap001files.storage.live.com/y4mN4YoeiPN4u7Hwy4M53v_6JcO9aCpxMeKsWTGVnkQZkZ3zm59Nb9icagtAdt6X-zTMuvkXo40x6nlviw-oTRj4GHOCojgIMy0gQeIVhF4nPYCKng03eVV0XyGxP9e2nS3NZJBhfTiuK2eaqIQhe_6cCAFaaisIh21nhptZ5ou_jhwAl_wDiiko83_VvJqalVk?width=576&height=720&cropmode=none",
+            "form": "3"
+        }
+    ],
+    "title": "Cốc thủ dâm hai chiều Lu Lu ",
+    "producer": "Jiusheng Plastic Factory",
+    "madeof": "Sillicon TPE",
+    "type": "Thủ công",
+    "detail": "Âm hộ giả với các bé loli từ gia đình Onasista, được mô phỏng theo tử cung của phụ nữ.",
+    "userfor": "Nam",
+    "numberofprod": "20",
+    "price" : "30"
+},
+{
+    "id": "2",
+    "image": [
+        {
+            "url": "",
+            "form": ""
+        }, {
+            "url": "",
+            "form": ""
+        }, {
+            "url": "",
+            "form": ""
+        }
+    ],
+    "title": "",
+    "producer": "",
+    "madeof": "",
+    "type": "",
+    "detail": "",
+    "userfor": "",
+    "numberofprod": "20",
+    "price" : "30"
+}
+]
